a3a036107569d4c1a0b4e06413ec8679ec288128
Kursat Turgut <94705179+KurSHOTturgut@users.noreply.github.com>
2022-03-09 22:26:39 +0300

    VucutKitle

---
+++ kursat/main.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class vucutkitleindeksi {
+    public static void main(String[] args) {
+
+        double sonuc,a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Boyunuzu Giriniz:");
+        a=sc.nextDouble();
+        System.out.println("Kilonuzu Giriniz:");
+        b=sc.nextDouble();
+        sonuc=(b/(a*a));
+        System.out.println("Vücut kitle indeksiniz:"+sonuc);
+        if (sonuc>30){
+            System.out.println("Şişmansnız");
+        }
+        else if (sonuc<30 && sonuc>20){
+            System.out.println("Normalsiniz");
+        }
+        else {
+            System.out.println("Zayıfsınız :)");
+        }
+    }
+    /*instagtam kursattrgt*/ 
+}
